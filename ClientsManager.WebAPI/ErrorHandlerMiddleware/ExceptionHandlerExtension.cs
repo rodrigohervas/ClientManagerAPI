8a0cccad339fabd9b471f43e438d2de6ad8942ee
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-09-15 11:19:50 -0400

    Added Logging to ExceptionHandlerExtension and modified Startup class to manage exceptions in non-development environment.

---
+++ ClientsManager.WebAPI/ErrorHandlerMiddleware/ExceptionHandlerExtension.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ClientsManager.WebAPI.ErrorHandlerMiddleware
+{
+    /// <summary>
+    /// Class that extends UseExceptionHandler middleware for custom error handling before returning the Http Response, 
+    /// adding a middleware to the pipeline that will catch exceptions, log them, and re-execute the request in an alternate pipeline.
+    /// After access to the error this class implements the following error handling features:
+    /// 1. Error logging
+    /// 2. Custom Http Response error message (using ErrorDetails class).
+    /// </summary>
+    public static class ExceptionHandlerExtension
+    {
+        /// <summary>
+        /// Method that catches the exception, logs it, generates a custom Response and sends the response back to the client.
+        /// </summary>
+        /// <param name="app">IApplicationBuilder object</param>
+        /// <param name="logger">ILogger object</param>
+        public static void UseExceptionHandlerExtension(this IApplicationBuilder app, ILogger _logger)
+        {
+            //register the UseExceptionHandler middleware, and provide a lambda expression as a parameter
+            //the Error Handling configuration will be done with the lambda expression
+            app.UseExceptionHandler(errorApp => 
+            {
+                errorApp.Run(async context => 
+                {
+                    //set the StatusCode and Content Type of the Http Response object
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json"; //sets the Web API response content-type header as json
+
+                    //access the exception
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        //log the error
+                        _logger.LogError($"There was an exception: {contextFeature.Error}");
+
+                        //write the Http Response body, using ErrorDetails class
+                        await context.Response.WriteAsync(
+                            new ErrorDetails()
+                            {
+                                StatusCode = context.Response.StatusCode,
+                                Message = "Internal Server Error",
+                                InternalErrorMessage = contextFeature.Error.Message
+                            }.ToString()
+                            );
+                    }
+                });
+            });
+        }
+    }
+}
--- ClientsManager.WebAPI/Startup.cs
+++ ClientsManager.WebAPI/Startup.cs
@@ -90,23 +90,19 @@ namespace ClientsManager.WebAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> _logger)
         {
-            app.ConfigureExceptionHandler();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                _logger.LogInformation("Development Environment");
             }
-            else
+            else 
             {
-                //error middleware
-                app.UseExceptionHandler("/error");
+                //hook Exception Handler middleware extension to the pipeline
+                app.UseExceptionHandlerExtension(_logger);
             }
-
-            //add Exception Handler Middleware
-            //app.ConfigureExceptionHandler(logger);
-            app.ConfigureExceptionHandler();
+                        
 
             app.UseHttpsRedirection();
 
