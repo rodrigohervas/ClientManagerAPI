5877bd6e948cfd4969d3f6a29f739bb88107092d
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-09-16 13:24:24 -0400

    Added logging

--- ClientsManager.WebAPI/ValidationActionFiltersMiddleware/ClientIdValidator.cs
+++ ClientsManager.WebAPI/ValidationActionFiltersMiddleware/ClientIdValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace ClientsManager.WebAPI.ValidationActionFiltersMiddleware
 {
     public class ClientIdValidator : IActionFilter
     {
+        private readonly ILogger _logger;
+
+        public ClientIdValidator(ILogger<ClientIdValidator> logger)
+        {
+            _logger = logger;
+        }
         public void OnActionExecuting(ActionExecutingContext context)
         {
             //get the action client_id parameter
@@ -17,6 +24,7 @@ namespace ClientsManager.WebAPI.ValidationActionFiltersMiddleware
             //validate that the client_id is not null/zero
             if (queryStringId == 0 || queryStringId == null)
             {
+                _logger.LogError($"ClientIdValidator: Client_id is mandatory. Value received: {queryStringId}");
                 context.Result = new BadRequestObjectResult("Client_id is mandatory");
                 return;
             }
