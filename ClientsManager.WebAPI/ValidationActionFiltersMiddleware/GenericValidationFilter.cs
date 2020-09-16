6836207faa9db0cf211f95b35f318a88f7d8d8f9
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-09-16 13:28:20 -0400

    Added logging

--- ClientsManager.WebAPI/ValidationActionFiltersMiddleware/GenericValidationFilter.cs
+++ ClientsManager.WebAPI/ValidationActionFiltersMiddleware/GenericValidationFilter.cs
@@ -1,6 +1,7 @@
 ﻿using ClientsManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,20 @@ namespace ClientsManager.WebAPI.ValidationActionFiltersMiddleware
 {
     public class GenericValidationFilter : IActionFilter
     {
+        private readonly ILogger _logger;
+
+        public GenericValidationFilter(ILogger<GenericValidationFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnActionExecuting(ActionExecutingContext context)
         {
             var param = context.ActionArguments.FirstOrDefault(p => p.Value is object);
 
             if (param.Value == null)
             {
+                _logger.LogError($"GenericValidationFilter: {param.Key} is null. Value received: {param.Value}");
                 context.Result = new BadRequestObjectResult($"{param.Key} is null");
                 return;
             }
