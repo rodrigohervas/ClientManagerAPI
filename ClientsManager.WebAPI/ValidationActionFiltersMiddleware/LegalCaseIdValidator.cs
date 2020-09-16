f79aa2d73b7c51cd755d644239fca86678f7ae38
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-09-16 13:22:27 -0400

    Added logging

--- ClientsManager.WebAPI/ValidationActionFiltersMiddleware/LegalCaseIdValidator.cs
+++ ClientsManager.WebAPI/ValidationActionFiltersMiddleware/LegalCaseIdValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,13 @@ namespace ClientsManager.WebAPI.ValidationActionFiltersMiddleware
 {
     public class LegalCaseIdValidator : IActionFilter
     {
+        private readonly ILogger _logger;
+
+        public LegalCaseIdValidator(ILogger<LegalCaseIdValidator> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnActionExecuting(ActionExecutingContext context)
         {
             //get the action case_id parameter
@@ -17,6 +25,7 @@ namespace ClientsManager.WebAPI.ValidationActionFiltersMiddleware
             //validate that the case_id is not null/zero
             if (queryStringId == 0 || queryStringId == null)
             {
+                _logger.LogError($"LegalCaseIdValidator: Case_id is mandatory. Value received: {queryStringId}");
                 context.Result = new BadRequestObjectResult("Case_id is mandatory");
                 return;
             }
