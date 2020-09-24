2a3d2e4620b3886520eabc6c4b096a51a7797e9e
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-09-24 17:30:57 -0400

    Changed namespace

--- ClientsManager.WebAPI/ErrorHandlingMiddleware/ErrorDetails.cs
+++ ClientsManager.WebAPI/ErrorHandlingMiddleware/ErrorDetails.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ClientsManager.WebAPI.ErrorHandlerExtension
+namespace ClientsManager.WebAPI.ErrorHandlingMiddleware
 {
     public class ErrorDetails
     {
