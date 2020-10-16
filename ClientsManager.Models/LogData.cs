39e12454d481b53f5ece109e0a3447f207fba5ca
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-10-16 17:26:49 -0400

    Added LogData class

---
+++ ClientsManager.Models/LogData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientsManager.Models
+{
+    public class LogData
+    {
+        /*
+         * NewBillableActivity = billableActivity,
+                    Action = ControllerContext.ActionDescriptor.DisplayName,
+                    Verb = HttpContext.Request.Method,
+                    EndpointPath = HttpContext.Request.Path.Value,
+                    User = HttpContext.User.Claims.First(usr => usr.Type == "preferred_username").Value
+         */
+        public object Entity { get; set; }
+        public string Action { get; set; }
+        public string Verb { get; set; }
+        public string EndpointPath { get; set; }
+        public string User { get; set; }
+
+        public LogData(ControllerContext controllerContext, HttpContext httpContext)
+        {
+            Entity = new { test = "value" };
+            Action = controllerContext.ActionDescriptor.DisplayName;
+            Verb = httpContext.Request.Method;
+            EndpointPath = httpContext.Request.Path.Value;
+            User = httpContext.User.Claims.First(usr => usr.Type == "preferred_username").Value;
+        }
+    }
+}
